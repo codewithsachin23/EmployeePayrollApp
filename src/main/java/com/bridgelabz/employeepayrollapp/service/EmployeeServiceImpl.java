d88fe1b088bcee432a5d3ff57d88fe483b4baa8a
sachin shukla <sachinshukla232003@gmail.com>
2025-02-25 12:34:58 +0530

    [Sachin Shukla]Add.UC-7 Add lombok @slf4j annotation for logging

--- src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeServiceImpl.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeServiceImpl.java
@@ -3,17 +3,19 @@ package com.bridgelabz.employeepayrollapp.service;
 import com.bridgelabz.employeepayrollapp.DTO.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
     public List<Employee> getEmployee(){
+        log.info("Display all Employee that have in App");
         return employeeRepository.findAll();
     }
 
@@ -23,23 +25,27 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setName(employeeDTO.getName());
         employee.setSalary(employeeDTO.getSalary());
         employeeRepository.save(employee);
+        log.info("new Employee added");
         return "Created new Employee";
     }
     public Optional<Employee> getByID(Long id){
+        log.info("Getting Employee Details based on ID");
         return employeeRepository.findById(id);
     }
     public String update(Long id,EmployeeDTO employeeDTO){
         Optional<Employee>optionalEmployee=employeeRepository.findById(id);
         if (optionalEmployee.isPresent()){
-            Employee e=new Employee();
+            Employee e=optionalEmployee.get();
             e.setName(employeeDTO.getName());
             e.setSalary(employeeDTO.getSalary());
             employeeRepository.save(e);
+            log.info("Updated a existing Employee details");
             return "updated";
         }
         return null;
     }
     public void deleteById(Long id){
+        log.info("Employee details deleted");
         employeeRepository.deleteById(id);
     }
 
