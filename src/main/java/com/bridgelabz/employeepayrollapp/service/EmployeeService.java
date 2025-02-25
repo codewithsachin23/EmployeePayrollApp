a837b40459141365e6cc9fa2d435318560e5a2e6
sachin shukla <sachinshukla232003@gmail.com>
2025-02-25 16:47:40 +0530

    [Sachin Shukla]Add.merge

--- src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.employeepayrollapp.service;
 
 import com.bridgelabz.employeepayrollapp.DTO.EmployeeDTO;
+import com.bridgelabz.employeepayrollapp.coustomexception.UserNotFoundException;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 
 
@@ -10,7 +11,7 @@ import java.util.Optional;
 public interface EmployeeService {
   List<Employee> getEmployee();
   String create(EmployeeDTO employeeDTO);
-  Optional<Employee> getByID(Long id);
+  Optional<Employee> getByID(Long id)throws UserNotFoundException;
   String update(Long id,EmployeeDTO employeesDTO);
   void deleteById(Long id);
 }
